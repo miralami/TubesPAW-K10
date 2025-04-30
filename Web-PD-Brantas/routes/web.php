53a8aa5b1de476ecc580806dd805fb6ff2422e56
dwiraniame <dwiraniamelia171@gmail.com>
2025-04-30 08:55:47 +0700

    ftur login
    fitur login tapi blm ada register

--- Web-PD-Brantas/routes/web.php
+++ Web-PD-Brantas/routes/web.php
@@ -1,21 +1,27 @@
+@ -1,6 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-// use App\Http\Controllers\AdminController; // kalau belum ada controllernya, aku sekalian buatin contoh
+use App\Http\Controllers\SesiController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\CatalogController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CheckoutController;
 
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', [SesiController::class, 'index'])->name('login');
+    Route::post('/', [SesiController::class, 'login']);
+});
+
+Route::get('/home', function () {
+    return redirect('/admin');
+});
+
+
 Route::resource('products', ProductController::class);
-
 Route::get('/', [LandingPageController::class, 'index'])->name('landing.index');
-
-// Route::middleware(['auth', 'admin'])->group(function () {
-//     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-// });
-
 Route::get('/catalog', [CatalogController::class, 'index'])->name('catalog.index');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
 Route::post('/cart/add/{productId}', [CartController::class, 'addToCart'])->name('cart.add');
@@ -23,3 +29,11 @@ Route::get('/cart', [CartController::class, 'viewCart'])->name('cart.view');
 Route::post('/cart/update/{id}', [CartController::class, 'updateQuantity'])->name('cart.update');
 Route::post('/cart/remove/{id}', [CartController::class, 'removeFromCart'])->name('cart.remove');
 Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index']);
+    Route::get('/admin/admin', [AdminController::class, 'admin']);
+    Route::get('/admin/user', [AdminController::class, 'user']);
+    Route::get('/logout', [SesiController::class, 'logout']);
+
+});
