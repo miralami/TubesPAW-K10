1d3523a29ebad1c1f95df780f4425cfbbebd6370
miralami <afifnursena21@gmail.com>
2025-05-08 23:50:54 +0700

    benerin products untuk non admin

--- Web-PD-Brantas/resources/views/layouts/admin.blade.php
+++ Web-PD-Brantas/resources/views/layouts/admin.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <style>
         body {
             overflow-x: hidden;
@@ -74,6 +75,6 @@
 </div>
 
 <!-- Bootstrap JS CDN -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
