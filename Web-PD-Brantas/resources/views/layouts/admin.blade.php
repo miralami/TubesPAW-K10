6b6dfd871eb488ee1b24687a4feb5afb9239652f
Afif Nur Sena <86998574+miralami@users.noreply.github.com>
2025-05-08 23:52:35 +0700

    Revert "benerin products untuk non admin"
    This reverts commit 1d3523a29ebad1c1f95df780f4425cfbbebd6370.

--- Web-PD-Brantas/resources/views/layouts/admin.blade.php
+++ Web-PD-Brantas/resources/views/layouts/admin.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <style>
         body {
             overflow-x: hidden;
@@ -75,6 +74,6 @@
 </div>
 
 <!-- Bootstrap JS CDN -->
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
